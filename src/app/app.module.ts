33d5ef4bb541d6eb1bb5db838ef834b9327be478
Shaikh Sadi <shaikhsadi049@gmail.com>
2021-10-26 21:59:35 +0600

    4th class work on add tutorial + add bootstap pacjage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +16,7 @@ import { TutorialsListComponent } from './components/tutorials-list/tutorials-li
     TutorialsListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Tutorial } from '../model/tutorial.model';
 
 @Component({
   selector: 'app-add-tutorial',
   templateUrl: './add-tutorial.component.html',
-  styleUrls: ['./add-tutorial.component.scss']
+  styleUrls: ['./add-tutorial.component.scss'],
 })
 export class AddTutorialComponent implements OnInit {
+  // submitted: boolean;
+  // submitted: boolean = false;
+  submitted = false;
 
-  constructor() { }
+  tutorial: Tutorial = {
+    title: '',
+    description: '',
+    published: false,
+  };
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // saveTutorial(): void {
+  saveTutorial() {
+    let data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description,
+    };
+
+    // this.tutorialService.create(data).subscribe(
+    //   (response) => {
+    //     console.log(response);
+    //     this.submitted = true;
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
   }
-
 }
